ed16da1c194dfffa3dd4a869ca5a06555fc76d27
rahul kathait <edendattox@gmail.com>
2021-02-25 20:52:43 +0530

    building header

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBXYIUnEbDQuasUDwaAjAQy8DVBwlbkCFk",
+    authDomain: "react-slack-998ab.firebaseapp.com",
+    projectId: "react-slack-998ab",
+    storageBucket: "react-slack-998ab.appspot.com",
+    messagingSenderId: "336863382418",
+    appId: "1:336863382418:web:ffe7ea35ed56e8dbb48f4b",
+    measurementId: "G-QWC67BY35S"
+  };
